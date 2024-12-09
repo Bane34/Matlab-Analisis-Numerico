5f67a89e2c7b3a9ecb811f37edc835e0df2302d8
Bane34 <bane34.42@gmail.com>
2024-12-09 20:27:02 +0100

    Finalizada la práctica

--- Lab 3/hermite.m
+++ Lab 3/hermite.m
@@ -7,17 +7,24 @@ function [c, Py] = hermite(x, f, fp, y)
 
     dif_div = zeros(2 * n, 1);
     dif_div(1) = f(1);
-    dif_div(2 : 2 : 2 * n) = fp
+    dif_div(2 : 2 : 2 * n) = fp;
    
 
     % Segunda columna de la tabla de diferencias divididas
     dif_div(3 : 2 : 2 * n - 1) = (f(2 : n) - f(1 : n - 1)) ./ (x(2 : n) - x(1 : n - 1));
-    coef(2) = dif_div(2);
 
     % Resto de columnas de la tabla de diferencias divididas
     for i = 3 : 2 * n 
         dif_div(i : 2 * n) = (dif_div(i : 2 * n) - dif_div(i - 1 : 2 * n - 1)) ./ (x_dup(i : 2 * n) - x_dup(1 : 2 * n - i + 1));
     end
     
+    % Usamos el algoritmo de Horner
+    m = length(y);
+    Py = dif_div(2 * n) * ones(1, m);
+
+    for j = 2 * n - 1 : -1 : 1
+        Py = Py .* (y - x_dup(j)) + dif_div(j);
+    end
+
     c = dif_div;
 end
--- Lab 3/runge_hermite.m
+++ Lab 3/runge_hermite.m
@@ -1,1 +1,46 @@
- K = [5, 9, 13, 17];
+%% @param
+% cond = 0 : se utilizan para la interpolacion los nodos de Chebyshev.
+% cond = 1 : se utilizan 501 nodos equiespaciados.
+
+
+function runge_hermite(cond)
+    K = [5, 9, 13, 17];
+
+    % Handle a la función de Runge
+    f = @(x) 1 ./ (1 + x.^2);
+    fp = @(x) -2 * x ./ [(1 + x.^2).^2];
+
+    % Dibujamos la función de Runge
+    f_vals = [-5 : (10 / 501) : 5];
+    r_vals = f(f_vals);
+    rp_vals = fp(f_vals);
+   
+    for j = 1 : 4
+        switch cond
+            case 0
+                nodos = 5 * (cos(( pi * (1 : 2 : 2 * K(j) - 1) )/ (2 * K(j)) ));
+                titulo = ' puntos de Chebyshev';
+            case 1
+                nodos = linspace(-5, 5, K(j));
+                titulo = ' abscisas equiespaciadas';
+        end
+
+        f_nodos = f(nodos);
+        fp_nodos = fp(nodos);
+
+        % Sacamos el polinomio interpolador
+        [~, Py] = hermite(nodos, f_nodos, fp_nodos, f_vals);
+        
+        hold
+        title(['funcion de Runge e interpolante de Hermite en ', num2str(K(j)), titulo])
+        plot(f_vals, r_vals, 'b-', f_vals, Py, 'r--', nodos, f_nodos, 'ro');
+        
+        for i = 1 : K(j)
+            plot([nodos(i) - 0.3, nodos(i) + 0.3], f_nodos(i) + [-0.3 * fp_nodos(i), 0.3 * fp_nodos(i)], 'k-');
+        end
+        hold
+        disp('Pulsar cualquier tecla para seguir')
+        pause
+    end
+
+end
